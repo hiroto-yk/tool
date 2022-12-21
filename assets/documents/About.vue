b90c3813b02d6f83c04cbed99df347aa658d557a
hiroto-yk <105357836+hiroto-yk@users.noreply.github.com>
2022-12-21 13:46:04 +0900

    update

--- assets/documents/About.vue
+++ assets/documents/About.vue
@@ -1,10 +1,7 @@
 <template>
   <div>
-    <h3>GSOL</h3>
-    <p>
-      A command-line utility to check the confluence of rewrite rules in a Haskell program using the tool Second-Order Laboratory (SOL).
-
-      gsol can check the termination and the local confluence of rewrite rules in a Haskell program.
+    <h3></h3>
+    <p></p>
     </p>
   </div>
 </template>
